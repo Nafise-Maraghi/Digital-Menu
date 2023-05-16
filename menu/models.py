f8961b6c3ee03f2221d8df654c43ea2f74b79989
Precious <nafiseh.maraghi@gmail.com>
2023-05-16 19:21:15 +0430

    models created

--- menu/models.py
+++ menu/models.py
@@ -1,1 +1,32 @@
 from django.db import models
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = "categories"
+
+    name = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Option(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    price = models.PositiveIntegerField(blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    availability = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items', blank=False, null=False)
+    description = models.CharField(max_length=250, help_text='ingredients', blank=True, null=True)
+    image = models.ImageField(upload_to="uploads/", blank=True, null=True)
+    name = models.CharField(max_length=100, blank=False, null=False)
+    price = models.PositiveIntegerField(blank=False, null=False)
+    options = models.ManyToManyField(Option, related_name='options', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
