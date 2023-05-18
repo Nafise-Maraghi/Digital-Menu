e52ae19d7361f9242ad90a21c0cbd709ba128651
Precious <nafiseh.maraghi@gmail.com>
2023-05-19 01:29:52 +0430

    admin panel activated

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,7 @@
+from .models import Category, Item, Option
 from django.contrib import admin
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(Option)
