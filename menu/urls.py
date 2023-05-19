b4e6daa113c92dc755a1e476077908a37202ef28
Precious <nafiseh.maraghi@gmail.com>
2023-05-19 19:53:08 +0430

    category urls fixed

--- menu/urls.py
+++ menu/urls.py
@@ -2,8 +2,8 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('/category/create/', CategoryCreateAPI.as_view()),
-    path('/category/list/', CategoryCreateAPI.as_view()),
-    path('/category/update/<int:pk>', CategoryCreateAPI.as_view()),
-    path('/category/delete/<int:pk>', CategoryCreateAPI.as_view()),
+    path('category/create/', CategoryCreateAPI.as_view()),
+    path('category/list/', CategoryListAPI.as_view()),
+    path('category/update/<int:pk>', CategoryUpdateAPI.as_view()),
+    path('category/delete/<int:pk>', CategoryDeleteAPI.as_view()),
 ]
