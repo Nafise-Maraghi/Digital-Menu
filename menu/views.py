ae492566b59aaf70f534d2a6deba53a1623ca12e
Precious <nafiseh.maraghi@gmail.com>
2023-05-22 14:53:10 +0430

    Create, Update, Delete Category APIs removed

--- menu/views.py
+++ menu/views.py
@@ -6,28 +6,9 @@ from rest_framework import generics
 
 # Category views
 
-# creating a single model instance
-class CategoryCreateAPI(generics.CreateAPIView):
-    model = Category
-    serializer_class = CategorySerializer
-
 
 # representing a collection of model instances
 class CategoryListAPI(generics.ListAPIView):
-    serializer_class = CategorySerializer
-    queryset = Category.objects.all()
-
-
-# updating a single model instance
-class CategoryUpdateAPI(generics.UpdateAPIView):
-    lookup_field = 'pk'
-    serializer_class = CategorySerializer
-    queryset = Category.objects.all()
-
-
-# deleting a single model instance
-class CategoryDeleteAPI(generics.DestroyAPIView):
-    lookup_field = 'pk'
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
 
