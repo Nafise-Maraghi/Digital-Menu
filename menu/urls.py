e9764060d818c448137ab192de87cd661925bac3
Precious <nafiseh.maraghi@gmail.com>
2023-05-30 16:49:47 +0430

    "Option" and "Category" urls added

--- menu/urls.py
+++ menu/urls.py
@@ -2,13 +2,24 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
+    # /category
+    path('category/create/', CategoryCreateAPI.as_view()),
+    path('category/list/', CategoryListAPI.as_view()),
+    path('category/list/<int:pk>/', CategoryUpdateAPI.as_view()),
+    path('category/update/<int:pk>/', CategoryUpdateAPI.as_view()),
+    path('category/delete/<int:pk>/', CategoryDeleteAPI.as_view()),
+
+    # /item
     path('item/create/', ItemCreateAPI.as_view()),
     path('item/list/', ItemListAPI.as_view()),
     path('item/list/<int:pk>/', ItemRetrieveAPI.as_view()),
     path('item/update/<int:pk>/', ItemUpdateAPI.as_view()),
     path('item/delete/<int:pk>/', ItemDeleteAPI.as_view()),
-    # path('option/create/', OptionCreateAPI.as_view()),
-    # path('option/list/', OptionListAPI.as_view()),
-    # path('option/update/<int:pk>/', OptionUpdateAPI.as_view()),
-    # path('option/delete/<int:pk>/', OptionDeleteAPI.as_view()),
+
+    # /option
+    path('option/create/', OptionCreateAPI.as_view()),
+    path('option/list/', OptionListAPI.as_view()),
+    path('option/list/<int:pk>/', OptionUpdateAPI.as_view()),
+    path('option/update/<int:pk>/', OptionUpdateAPI.as_view()),
+    path('option/delete/<int:pk>/', OptionDeleteAPI.as_view()),
 ]
