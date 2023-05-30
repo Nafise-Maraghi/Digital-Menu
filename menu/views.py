d370ebd5b4d98d87236f50c55553292614a262a0
Precious <nafiseh.maraghi@gmail.com>
2023-05-30 16:46:40 +0430

    "Option" and "Category" views added

--- menu/views.py
+++ menu/views.py
@@ -1,46 +1,86 @@
-from .models import Category, Item
-from .serializers import ItemSerializer
+from .models import Category, Item, Option
+from .serializers import ItemSerializer, OptionSerializer, CategorySerializer
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
+# Category views
+
+# creating a single model instance
+class CategoryCreateAPI(generics.CreateAPIView):
+    model = Category
+    permission_classes = [IsAuthenticated]
+    serializer_class = CategorySerializer
+
+
+# representing a single model instance
+class CategoryRetrieveAPIView(generics.RetrieveAPIView):
+    lookup_field = 'pk'
+    permission_classes = [IsAuthenticated]
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+# representing a collection of model instances
+class CategoryListAPI(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+# updating a single model instance
+class CategoryUpdateAPI(generics.UpdateAPIView):
+    lookup_field = 'pk'
+    permission_classes = [IsAuthenticated]
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+# deleting a single model instance
+class CategoryDeleteAPI(generics.DestroyAPIView):
+    lookup_field = 'pk'
+    permission_classes = [IsAuthenticated]
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+# ______________________________
 
 # Item views
 
 # creating a single model instance
 class ItemCreateAPI(generics.CreateAPIView):
-    permission_classes = [IsAuthenticated]
     model = Item
+    permission_classes = [IsAuthenticated]
     serializer_class = ItemSerializer
 
 
 # representing a single model instance
 class ItemRetrieveAPI(generics.RetrieveAPIView):
-    serializer_class = ItemSerializer
     lookup_field = 'pk'
     queryset = Item.objects.all()
+    serializer_class = ItemSerializer
 
 
 # representing a collection of model instances
 class ItemListAPI(generics.ListAPIView):
-    serializer_class = ItemSerializer
     queryset = Item.objects.all()
+    serializer_class = ItemSerializer
 
 
 # updating a single model instance
 class ItemUpdateAPI(generics.UpdateAPIView):
-    permission_classes = [IsAuthenticated]
     lookup_field = 'pk'
-    serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticated]
     queryset = Item.objects.all()
+    serializer_class = ItemSerializer
 
 
 # deleting a single model instance
 class ItemDeleteAPI(generics.DestroyAPIView):
-    permission_classes = [IsAuthenticated]
     lookup_field = 'pk'
-    serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticated]
     queryset = Item.objects.all()
+    serializer_class = ItemSerializer
 
 
 # ______________________________
@@ -48,30 +88,30 @@ class ItemDeleteAPI(generics.DestroyAPIView):
 # Option views
 
 # creating a single model instance
-# class OptionCreateAPI(generics.CreateAPIView):
-#     permission_classes = [IsAuthenticated]
-#     model = Option
-#     serializer_class = OptionSerializer
+class OptionCreateAPI(generics.CreateAPIView):
+    model = Option
+    permission_classes = [IsAuthenticated]
+    serializer_class = OptionSerializer
 
 
 # # representing a collection of model instances
-# class OptionListAPI(generics.ListAPIView):
-#     permission_classes = [IsAuthenticated]
-#     serializer_class = OptionSerializer
-#     queryset = Option.objects.all()
+class OptionListAPI(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Option.objects.all()
+    serializer_class = OptionSerializer
 
 
 # # updating a single model instance
-# class OptionUpdateAPI(generics.UpdateAPIView):
-#     permission_classes = [IsAuthenticated]
-#     lookup_field = 'pk'
-#     serializer_class = OptionSerializer
-#     queryset = Option.objects.all()
+class OptionUpdateAPI(generics.UpdateAPIView):
+    lookup_field = 'pk'
+    permission_classes = [IsAuthenticated]
+    queryset = Option.objects.all()
+    serializer_class = OptionSerializer
 
 
 # # deleting a single model instance
-# class OptionDeleteAPI(generics.DestroyAPIView):
-#     permission_classes = [IsAuthenticated]
-#     lookup_field = 'pk'
-#     serializer_class = OptionSerializer
-#     queryset = Option.objects.all()
+class OptionDeleteAPI(generics.DestroyAPIView):
+    lookup_field = 'pk'
+    permission_classes = [IsAuthenticated]
+    queryset = Option.objects.all()
+    serializer_class = OptionSerializer
