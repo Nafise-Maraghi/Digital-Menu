a3e677da485149323b9b53418f99be4e02578cec
Precious <nafiseh.maraghi@gmail.com>
2023-05-30 16:23:49 +0430

    "Option" and "Category" serializers added

--- menu/serializers.py
+++ menu/serializers.py
@@ -1,11 +1,17 @@
-from .models import Category, Item
+from .models import Category, Item, Option
 from rest_framework import serializers
 
 
-# class OptionSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Option
-#         fields = '__all__'
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class OptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Option
+        fields = '__all__'
 
 
 class ItemSerializer(serializers.ModelSerializer):
