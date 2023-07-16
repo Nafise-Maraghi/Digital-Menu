a5f56075067f585d015e497be4365660a20d1977
Precious <nafiseh.maraghi@gmail.com>
2023-07-16 16:01:32 +0430

    staticfiles added and project configed to deploy to railway

--- main/settings.py
+++ main/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-ksb*o%e10wo#tn*lq(e!eqc9rv&n7g(#6pyzs%s@ut!%l38owb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
